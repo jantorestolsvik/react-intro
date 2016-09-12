d195c75193a0c41e61438a52beab5be93b00eed5
Jan Tore Stølsvik <jan.tore@stoelsvik.no>
2016-09-12 19:30:14 +0200

    Static content

--- src/main.js
+++ src/main.js
@@ -4,7 +4,90 @@ import ReactDOM from 'react-dom';
 let TodoApp = React.createClass({
     render: function () {
         return (
-            <h1>Hello World</h1>
+            <div>
+                <Header />
+                <section className="main">
+                    <input
+                        className="toggle-all"
+                        type="checkbox"
+                    />
+                    <ul className="todo-list">
+                        <TodoItem />
+                    </ul>
+                </section>
+                <Footer />
+            </div>
+        );
+    }
+});
+
+let Header = React.createClass({
+    render: function () {
+        return (
+            <header className="header">
+                <h1>todos</h1>
+                <input
+                    className="new-todo"
+                    placeholder="What needs to be done?"
+                />
+            </header>
+        );
+    }
+});
+
+let TodoItem = React.createClass({
+    render: function () {
+        return (
+            <li>
+                <div className="view">
+                    <input
+                        className="toggle"
+                        type="checkbox"
+                    />
+                    <label>
+                        Test todo
+                    </label>
+                    <button className="destroy"/>
+                </div>
+                <input
+                    className="edit"
+                />
+            </li>
+        );
+    }
+});
+
+let Footer = React.createClass({
+    render: function () {
+        return (
+            <footer className="footer">
+                <span className="todo-count">
+                    <strong>123</strong> items left
+                </span>
+                <ul className="filters">
+                    <li>
+                        <a>
+                            All
+                        </a>
+                    </li>
+                    {' '}
+                    <li>
+                        <a>
+                            Active
+                        </a>
+                    </li>
+                    {' '}
+                    <li>
+                        <a>
+                            Completed
+                        </a>
+                    </li>
+                </ul>
+                <button
+                    className="clear-completed">
+                    Clear completed
+                </button>
+            </footer>
         );
     }
 });
